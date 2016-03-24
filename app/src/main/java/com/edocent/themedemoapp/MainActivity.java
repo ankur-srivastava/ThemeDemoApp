c3b2f7d6bb2058a43cc5d3f66f745cc1b4a89b67
Ankur Srivastava <ankurlkw@gmail.com>
2016-03-24 07:41:05 +0530

    Share Intent

--- app/src/main/java/com/edocent/themedemoapp/MainActivity.java
+++ app/src/main/java/com/edocent/themedemoapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.edocent.themedemoapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ShareActionProvider;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,20 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        //Share Action Provider
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        mShareActionProvider = (ShareActionProvider)menuItem.getActionProvider();
+        setIntent("Testing Share Feature");
+
         return true;
+    }
+
+    private void setIntent(String s) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/html");
+        intent.putExtra(Intent.EXTRA_TEXT, s);
+        mShareActionProvider.setShareIntent(intent);
     }
 
     @Override
@@ -27,9 +45,10 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id){
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
